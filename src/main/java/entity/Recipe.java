1ee09787c5d27be7a7aa069f0dcd1a70e6811666
YCJiVanessa <yuchen.ji@mail.utoronto.ca>
2025-11-14 17:47:26 -0500

    update

--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -1,6 +1,44 @@
 package entity;
 
-public class Recipe {
+import java.util.List;
 
+public class Recipe {
+    private final String title;
+    private final List<String> ingredientNames;
+    private final int calories;
+    private final int healthScore;
+    private final String instructions;
+
+    public Recipe(String title,
+                  List<String> ingredientNames,
+                  int calories,
+                  int healthScore,
+                  String instructions) {
+        this.title = title;
+        this.ingredientNames = ingredientNames;
+        this.calories = calories;
+        this.healthScore = healthScore;
+        this.instructions = instructions;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<String> getIngredientNames() {
+        return ingredientNames;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getHealthScore() {
+        return healthScore;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+}
 
