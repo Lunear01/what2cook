6268c053dec1e42da2be43bbc027f0b9ea52e134
MohanYuan2005 <mohan.yuan@mail.utoronto.ca>
2025-11-21 20:14:19 -0500

    update

--- src/main/java/interface_adapter/recipe_search/RecipeSearchViewModel.java
+++ src/main/java/interface_adapter/recipe_search/RecipeSearchViewModel.java
@@ -2,9 +2,58 @@ package interface_adapter.recipe_search;
 
 import interface_adapter.ViewModel;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Ingredient;
+import entity.Recipe;
+
 public class RecipeSearchViewModel extends ViewModel<RecipeSearchState> {
+
+    public static final String VIEW_NAME = "recipe search";
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
     public RecipeSearchViewModel() {
-        super("recipe search");
+        super(VIEW_NAME);
         setState(new RecipeSearchState());
+    }
+
+    /** 添加监听器（UI 用）*/
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    /** 通知 UI 更新*/
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, getState());
+    }
+
+    /** 设置 ingredients（从 IngredientSearchView 传进来）*/
+    public void setCurrentIngredients(List<Ingredient> ingredients) {
+        RecipeSearchState newState = new RecipeSearchState(getState());
+        newState.setIngredients(new ArrayList<>(ingredients));
+        setState(newState);
+        firePropertyChanged();
+    }
+
+    /** 设置搜索出来的 recipes */
+    public void setRecipes(List<Recipe> recipes) {
+        RecipeSearchState newState = new RecipeSearchState(getState());
+        newState.setRecipes(new ArrayList<>(recipes));
+        setState(newState);
+        firePropertyChanged();
+    }
+
+    /** 设置错误信息 */
+    public void setError(String error) {
+        RecipeSearchState newState = new RecipeSearchState(getState());
+        newState.setError(error);
+        setState(newState);
+        firePropertyChanged();
     }
 }
