dd999e802d243d48653916417c97ff262a5d0dad
David Kim <davidkangheekim@gmail.com>
2025-11-29 15:37:55 -0500

    Refactored to: Change class names to be consistent accross the project. Deleted unused classes and a package.
    handled checked exceptions for
    IngredientDataAccessInterface,
    IngredientDataAccessObject.
    Refactored variable names for style.

--- src/main/java/dataaccess/IngredientDataAccessInterface.java
+++ src/main/java/dataaccess/IngredientDataAccessInterface.java
@@ -7,24 +7,24 @@ import java.util.List;
 public interface IngredientDataAccessInterface {
     /**
      * add the ingredient.
-     * @param user_name the name of current user
+     * @param userName the name of current user
      * @param ingredientID the id of the ingredient that need to be added
-     * @param ingredient_name the name of the ingredient that need to be added
+     * @param ingredientName the name of the ingredient that need to be added
      */
-    void addIngredient(String user_name, int ingredientID, String ingredient_name) throws Exception;
+    void addIngredient(String userName, int ingredientID, String ingredientName);
 
     /**
      * get the ingredient.
-     * @param user_name the name of the current user
+     * @param userName the name of the current user
      * @return the list of the ingredients that was created by user.
      */
-    List<Ingredient> getAllIngredients(String user_name) throws Exception;
+    List<Ingredient> getAllIngredients(String userName);
 
     /**
      * delete the ingredient.
-     * @param user_name the name of current user
+     * @param userName the name of current user
      * @param ingredientID the id of the ingredient that need to be deleted
      */
-    void deleteIngredient(String user_name, int ingredientID) throws Exception;
+    void deleteIngredient(String userName, int ingredientID);
 
 }
--- src/main/java/dataaccess/IngredientDataAccessObject.java
+++ src/main/java/dataaccess/IngredientDataAccessObject.java
@@ -12,90 +12,109 @@ import java.util.List;
 
 public class IngredientDataAccessObject implements IngredientDataAccessInterface {
 
-    private final String baseUrl = "http://localhost:3000/ingredient";
+    private static final String BASE_URL = "http://localhost:3000/ingredient";
 
     @Override
-    public void addIngredient(String user_name, int ingredientID, String ingredient_name) throws Exception {
+    public void addIngredient(String userName, int ingredientID, String ingredientName) {
+        try {
+            final URL url = new URL(BASE_URL + "/add");
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        final URL url = new URL(baseUrl + "/add");
-        final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
 
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setDoOutput(true);
+            final JSONObject body = new JSONObject();
+            body.put("user_name", userName);
+            body.put("ingredient_name", ingredientName);
+            body.put("ingredient_id", ingredientID);
 
-        final JSONObject body = new JSONObject();
-        body.put("user_name", user_name);
-        body.put("ingredient_name", ingredient_name);
-        body.put("ingredient_id", ingredientID);
+            final OutputStream os = conn.getOutputStream();
+            os.write(body.toString().getBytes());
+            os.flush();
 
-        final OutputStream os = conn.getOutputStream();
-        os.write(body.toString().getBytes());
-        os.flush();
-
-        conn.getInputStream();
+            conn.getInputStream();
+        }
+        catch (IOException ioException) {
+            System.out.println("Error opening connection: " + ioException.getMessage());
+            ioException.printStackTrace();
+            throw new RuntimeException(ioException);
+        }
     }
 
     @Override
-    public List<Ingredient> getAllIngredients(String user_name) throws Exception {
+    public List<Ingredient> getAllIngredients(String userName) {
+        try {
+            final URL url = new URL(BASE_URL + "/" + userName);
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        final URL url = new URL(baseUrl + "/" + user_name);
-        final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
 
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setDoOutput(true);
+            final JSONObject body = new JSONObject();
+            body.put("user_name", userName);
 
-        final JSONObject body = new JSONObject();
-        body.put("user_name", user_name);
+            final OutputStream os = conn.getOutputStream();
+            os.write(body.toString().getBytes());
+            os.flush();
 
-        final OutputStream os = conn.getOutputStream();
-        os.write(body.toString().getBytes());
-        os.flush();
+            final BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            final StringBuilder sb = new StringBuilder();
+            String line;
 
-        final BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        final StringBuilder sb = new StringBuilder();
-        String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
 
-        while ((line = br.readLine()) != null) {
-            sb.append(line);
+            final JSONObject res = new JSONObject(sb.toString());
+
+            final JSONArray ingredientsArray = res.getJSONArray("ingredients");
+
+            final List<Ingredient> ingredientList = new ArrayList<>();
+
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                final JSONObject obj = ingredientsArray.getJSONObject(i);
+                final Ingredient ing = Ingredient.builder()
+                        .setName(obj.getString("ingredient_name"))
+                        .setId(obj.getInt("ingredient_id"))
+                        .build();
+                ingredientList.add(ing);
+            }
+            return ingredientList;
         }
-
-        final JSONObject res = new JSONObject(sb.toString());
-
-        final JSONArray ingredientsArray = res.getJSONArray("ingredients");
-
-        final List<Ingredient> ingredientList = new ArrayList<>();
-
-        for (int i = 0; i < ingredientsArray.length(); i++) {
-            final JSONObject obj = ingredientsArray.getJSONObject(i);
-            final Ingredient ing = Ingredient.builder()
-                    .setName(obj.getString("ingredient_name"))
-                    .setId(obj.getInt("ingredient_id"))
-                    .build();
-            ingredientList.add(ing);
+        catch (IOException ioException) {
+            System.out.println("Error opening connection: " + ioException.getMessage());
+            ioException.printStackTrace();
+            throw new RuntimeException(ioException);
         }
-
-        return ingredientList;
     }
 
     @Override
-    public void deleteIngredient(String user_name, int ingredientID) throws Exception {
-        final URL url = new URL(baseUrl + "/delete");
-        final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    public void deleteIngredient(String userName, int ingredientID) {
+        try {
+            final URL url = new URL(BASE_URL + "/delete");
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        conn.setRequestMethod("DELETE");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setDoOutput(true);
+            conn.setRequestMethod("DELETE");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
 
-        final JSONObject body = new JSONObject();
-        body.put("user_name", user_name);
-        body.put("ingredient_id", ingredientID);
+            final JSONObject body = new JSONObject();
+            body.put("user_name", userName);
+            body.put("ingredient_id", ingredientID);
 
-        final OutputStream os = conn.getOutputStream();
-        os.write(body.toString().getBytes());
-        os.flush();
+            final OutputStream os = conn.getOutputStream();
+            os.write(body.toString().getBytes());
+            os.flush();
 
-        conn.getInputStream();
+            conn.getInputStream();
+        }
+        catch (IOException ioException) {
+            System.out.println("Error opening connection: " + ioException.getMessage());
+            ioException.printStackTrace();
+            throw new RuntimeException(ioException);
+        }
+
     }
 }
