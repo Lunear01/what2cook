95eaa1c58af45dbd2d05e8b52d934a610097c018
Lotus <hankong.kong@mail.utoronto.ca>
2025-11-29 21:03:39 -0500

    cryber

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,16 @@
 const userServices = require("../services/userServices");
 // const db = requir("../config/db");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res) => {
     try {
         const { user_name, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         const exists = await userServices.findUser(user_name);
         if (exists) return res.status(400).json({ message: "User already exists" });
 
-        await userServices.createUser(user_name, email, password);
+        await userServices.createUser(user_name, email, hashedPassword);
         res.json({ message: "User registered" });
     } catch (err) {
         console.error(err);
@@ -44,8 +46,11 @@ exports.login = async (req, res) => {
         const user = await userServices.findUser(user_name);
         if (!user) return res.status(400).json({ message: "User not found" });
 
-        if (user.password !== password)
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
             return res.status(400).json({ message: "Wrong password" });
+        }
 
         res.json({ message: "Login success", user });
     } catch (err) {
