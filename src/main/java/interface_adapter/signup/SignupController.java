ada09dfa060d12d79b31920c76491c10354f4ce2
YCJiVanessa <yuchen.ji@mail.utoronto.ca>
2025-12-01 14:29:24 -0500

    checkstyle

--- src/main/java/interface_adapter/signup/SignupController.java
+++ src/main/java/interface_adapter/signup/SignupController.java
@@ -3,15 +3,41 @@ package interface_adapter.signup;
 import use_case.signup.SignupInputBoundary;
 import use_case.signup.SignupInputData;
 
+/**
+ * Controller for handling user signup actions.
+ * Converts raw input from the UI into {@link SignupInputData} and
+ * forwards it to the signup use case interactor.
+ */
 public class SignupController {
+
     private final SignupInputBoundary interactor;
 
-    public SignupController(SignupInputBoundary interactor) {
+    /**
+     * Constructs a SignupController with the given interactor.
+     *
+     * @param interactor the signup input boundary implementation
+     */
+    public SignupController(final SignupInputBoundary interactor) {
         this.interactor = interactor;
     }
 
-    public void signup(String username, String email, String password, String confirmPassword) {
-        final SignupInputData input = new SignupInputData(username, email, password, confirmPassword);
+    /**
+     * Handles the signup request coming from the UI layer.
+     *
+     * @param username        the username entered by the user
+     * @param email           the email entered by the user
+     * @param password        the password entered by the user
+     * @param confirmPassword the confirmation of the password
+     */
+    public void signup(
+            final String username,
+            final String email,
+            final String password,
+            final String confirmPassword
+    ) {
+        final SignupInputData input =
+                new SignupInputData(username, email, password, confirmPassword);
+
         interactor.execute(input);
     }
 }
